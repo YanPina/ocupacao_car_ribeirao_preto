6dbb706d69aa08757c27f855c23cb6b4549745d3
YanPina <yanpina85@gmail.com>
2021-10-01 01:37:13 -0300

    map

--- admin/src/pages/admin/dashboard/index.js
+++ admin/src/pages/admin/dashboard/index.js
@@ -115,7 +115,7 @@ export default function Dashboard() {
                       {...viewport}
                       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
                       onViewportChange={nextViewport => setViewport(nextViewport)}
-                      mapStyle='mapbox://styles/yanpina/cku4pfpr80xvp17qonyh9oevt'
+                      mapStyle='mapbox://styles/yanpina/cku7vbiuq46tp19qj8om6ajyv'
                   />
               </Paper>
             </Grid>
--- admin/src/pages/admin/dashboard/options_chart.js
+++ admin/src/pages/admin/dashboard/options_chart.js
@@ -1,12 +1,16 @@
 const chartOptions = {
     options: {
-        colors: ['#00ff0d', '#1f1745', '#ff0000'],
+        colors: ['#00ff0d', '#1f1745', '#ff0000', '#00ccff'],
         chart: {
           type: 'bar',
           width:'100%'
         },
-        labels: ['Ocupação Total', 'Ocupação Ativos', 'Ocupação Cancelados'],
+        labels: ['Ocupação Total', 'Área de Preservação', 'Reserva Legal', 'Vegetação Nativa'],
         dataLabels: {
+          style: {
+            fontSize: '12px',
+            colors: ["#000000"]
+          },
           enabled: true,
           formatter: function (val) {
             return val + "%"
